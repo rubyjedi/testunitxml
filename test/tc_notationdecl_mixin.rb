807fc1fa14b561cafd78358260fd620f5296109f
henrik <henrik@2ace18fd-e209-0410-b992-a826f012da46>
2006-01-29 06:35:25 +0000

    Just refactored the test a bit.
    git-svn-id: http://testunitxml.rubyforge.org/svn/trunk@24 2ace18fd-e209-0410-b992-a826f012da46

--- test/tc_notationdecl_mixin.rb
+++ test/tc_notationdecl_mixin.rb
@@ -12,15 +12,26 @@ require 'test/unit/xml/notationdecl_mixin'
 class TestNotationDecl < Test::Unit::TestCase
 
   def setup
-    doc_string = <<-'XMLEND'
+    @pubid1 = "TEST1"
+    @pubid2 = "TEST2"
+    @sysid2 = "urn:x-henrikmartensson.org:test2"
+    @pubid3 = "TEST3"
+    @pubid4 = "TEST4"
+    @sysid4 = "urn:x-henrikmartensson.org:test4"
+    @pubid5 = "TEST5"
+    @sysid5 = "urn:x-henrikmartensson.org:test5"
+    @pubid6 = "TEST6"
+    @sysid6 = "urn:x-henrikmartensson.org:test6"
+    @sysid7 = "urn:x-henrikmartensson.org:test7"
+    doc_string = <<-"XMLEND"
     <!DOCTYPE r SYSTEM "urn:x-henrikmartensson:test" [
-      <!NOTATION n1 PUBLIC "TEST1">
-      <!NOTATION n2 PUBLIC "TEST2" "urn:x-henrikmartensson.org:test2">
-      <!NOTATION n3 PUBLIC 'TEST3'>
-      <!NOTATION n4 PUBLIC 'TEST4' 'urn:x-henrikmartensson.org:test4'>
-      <!NOTATION n5 PUBLIC "TEST5" 'urn:x-henrikmartensson.org:test5'>
-      <!NOTATION n6 PUBLIC 'TEST6' "urn:x-henrikmartensson.org:test6">
-      <!NOTATION n7 SYSTEM "urn:x-henrikmartensson.org:test7">
+      <!NOTATION n1 PUBLIC "#{@pubid1}">
+      <!NOTATION n2 PUBLIC "#{@pubid2}" "#{@sysid2}">
+      <!NOTATION n3 PUBLIC '#{@pubid3}'>
+      <!NOTATION n4 PUBLIC '#{@pubid4}' '#{@sysid4}'>
+      <!NOTATION n5 PUBLIC "#{@pubid5}" '#{@sysid5}'>
+      <!NOTATION n6 PUBLIC '#{@pubid6}' "#{@sysid6}">
+      <!NOTATION n7 SYSTEM "#{@sysid7}">
     ]>
     <r/>
     XMLEND
@@ -32,26 +43,26 @@ class TestNotationDecl < Test::Unit::TestCase
   end
   
   def test_public
-    assert_equal('TEST1', @doctype.notation('n1').public)
-    assert_equal('TEST2', @doctype.notation('n2').public)
-    assert_equal('TEST3', @doctype.notation('n3').public)
-    assert_equal('TEST4', @doctype.notation('n4').public)
+    assert_equal(@pubid1, @doctype.notation('n1').public)
+    assert_equal(@pubid2, @doctype.notation('n2').public)
+    assert_equal(@pubid3, @doctype.notation('n3').public)
+    assert_equal(@pubid4, @doctype.notation('n4').public)
     # The TEST5 and TEST6 assertions have been commented out because
     # REXML 3.1.2 does not parse the notations correctly.
-    #assert_equal('TEST5', @doctype.notation('n5').public)
-    #assert_equal('TEST6', @doctype.notation('n6').public)
+    #assert_equal(@pubid5, @doctype.notation('n5').public)
+    #assert_equal(@pubid6, @doctype.notation('n6').public)
     assert_nil(@doctype.notation('n7').public)
   end
   
   def test_system
-    assert_equal("urn:x-henrikmartensson.org:test2", @doctype.notation('n2').system)
+    assert_equal(@sysid2, @doctype.notation('n2').system)
     assert_nil(@doctype.notation('n3').system)
-    assert_equal("urn:x-henrikmartensson.org:test4", @doctype.notation('n4').system)
+    assert_equal(@sysid4, @doctype.notation('n4').system)
     # The TEST5 and TEST6 assertions have been commented out because
     # REXML 3.1.2 does not parse the notations correctly.
-    #assert_equal('urn:x-henrikmartensson.org:test5', @doctype.notation('n5').system)
-    #assert_equal('urn:x-henrikmartensson.org:test6', @doctype.notation('n6').system)
-    assert_equal("urn:x-henrikmartensson.org:test7", @doctype.notation('n7').system)
+    #assert_equal(@sysid5, @doctype.notation('n5').system)
+    #assert_equal(@sysid6, @doctype.notation('n6').system)
+    assert_equal(@sysid7, @doctype.notation('n7').system)
   end
 
 end
